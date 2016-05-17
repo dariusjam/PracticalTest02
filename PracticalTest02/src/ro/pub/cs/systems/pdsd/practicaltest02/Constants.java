33d2a526488d723946284171a5979b6deed91b4d
Student User <student@cti.pub.ro>
2016-05-17 12:02:52 +0300

    Final

---
+++ PracticalTest02/src/ro/pub/cs/systems/pdsd/practicaltest02/Constants.java
@@ -0,0 +1,23 @@
+package ro.pub.cs.systems.pdsd.practicaltest02;
+
+public interface Constants {
+
+    final public static String TAG = "[PracticalTest02]";
+
+    final public static boolean DEBUG = true;
+
+    final public static String WEB_SERVICE_ADDRESS = "http://www.timeapi.org/utc/now";
+
+    final public static String KEY = "key";
+    final public static String VALUE = "value";
+
+    final public static String EMPTY_STRING = "";
+
+    final public static String QUERY_ATTRIBUTE = "query";
+
+    final public static String SCRIPT_TAG = "script";
+    final public static String SEARCH_KEY = "wui.api_data =\n";
+
+    final public static String CURRENT_OBSERVATION = "current_observation";
+
+}
--- PracticalTest02/src/ro/pub/cs/systems/pdsd/practicaltest02/PracticalTest02MainActivity.java
+++ PracticalTest02/src/ro/pub/cs/systems/pdsd/practicaltest02/PracticalTest02MainActivity.java
@@ -2,16 +2,73 @@ package ro.pub.cs.systems.pdsd.practicaltest02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class PracticalTest02MainActivity extends Activity {
+	
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientadresa = null;
+    private EditText clientport = null;
+    private EditText key = null;
+    private EditText value = null;
+    private EditText serverport = null;
+    private Button	connect = null;
+    private Button cerere = null;
+
+
+    private ServerThread serverThread = null;
+    //private ClientThread clientThread = null;
+
+    
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverport.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Server port should be filled!",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            serverThread = new ServerThread(Integer.parseInt(serverPort));
+            if (serverThread.getServerSocket() != null) {
+                serverThread.start();
+            } else {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not creat server thread!");
+            }
+
+        }
+    }
+    
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test02_main);
+        
+        connectButton = (Button)findViewById(R.id.button1);
+        connectButton.setOnClickListener(connectButtonClickListener);
+
+        clientadresa = (EditText)findViewById(R.id.address);
+        clientport = (EditText)findViewById(R.id.port2);
+        serverport = (EditText)findViewById(R.id.port1);
+        cerere= (Button)findViewById(R.id.button1);
+        //cerere.setOnClickListener(Cerere);
     }
 
 
@@ -32,5 +89,13 @@ public class PracticalTest02MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected void onDestroy() {
+        if (serverThread != null) {
+            serverThread.stopThread();
+        }
+        super.onDestroy();
     }
 }
